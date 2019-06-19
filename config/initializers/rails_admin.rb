61430dab8ba045e338865c837568945404015f57
Edgar Tomeyan <ed_tomeyan@mail.ru>
2019-06-20 01:41:08 +0300

    login & password access to admin panel

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,5 +1,11 @@
 RailsAdmin.config do |config|
 
+  config.authenticate_with do
+    authenticate_or_request_with_http_basic('Site Message') do |username, password|
+      username == 'frogogo' && password == 'asdfasdf'
+    end
+  end
+
   ### Popular gems integration
 
   ## == Devise ==

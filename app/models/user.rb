9045667834d55b29ca0bb51a92df4800cb68d395
Edgar Tomeyan <46032349+just-ed@users.noreply.github.com>
2019-06-20 11:42:08 +0300

    name and phone validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,18 @@ class User < ApplicationRecord
   
   has_secure_password
 
-  validates :phone_number, presence: true, uniqueness: true, numericality: true
-  validates :name, presence: true
+  validates :phone_number, presence: true, 
+                           uniqueness: true,
+                           numericality: true
+                           length: { minimum: 10 }
+  validates :name, presence: true, length: { minimum: 2 }
   validates :password,
             length: { minimum: 6 },
             if: -> { new_record? || !password.nil? }
+
+  before_validation :clean_number, only: :phone_number
+
+  def clean_number
+    self.phone_number.gsub!(/[\+\-\s()]/, "")
+  end
 end
